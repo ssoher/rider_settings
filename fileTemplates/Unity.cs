3ad3bc12a52d5838dc4be20b1ca111ead0e1ec65
sarpe <sarpe@DESKTOP-AEE>
2018-12-20 07:56:21 +0300

    RD-2018.3 <sarpe@DESKTOP-AEE Create ide.general.xml, Rider copy.xml, _@user_Default.icls, project.default.xml, diff.xml, other.xml, Default.xml, find.xml, ui.lnf.xml, keymap.xml, plugin_ui.xml, colors.scheme.xml, Defender3D_Header.cs, customization.xml, _@user_Visual Studio Light.icls, CLEAR.icls, Ergo.icls, laf.xml, KeyPromoterXStatistic.xml, Unity.cs, _@user_ReSharper Light.icls, codeLens.xml, editor.codeinsight.xml, _@user_ReSharper Dark.icls, editor.xml, usageView.xml, Sarper.icls, debugger.xml, updates.xml

---
+++ fileTemplates/Unity.cs
@@ -0,0 +1,10 @@
+#parse( "Defender3D_Header.cs" )
+
+
+namespace ${NAME} {
+    using UnityEngine;
+    
+    internal sealed class ${NAME} : MonoBehaviour {
+        
+    }
+}
---
+++ fileTemplates/includes/Defender3D_Header.cs
@@ -0,0 +1,10 @@
+#set( $PRODUCT_NAME = "Defender 3D" )
+#set( $DEVELOPER_NAME = "Sarper Soher" )
+#set( $COMPANY_NAME = "Accidental Empire Entertainment" )
+#set( $COMPANY_WEB = "http://www.accidentalee.com" )
+/*===============================================================
+Product:    ${PRODUCT_NAME}
+Developer:  ${DEVELOPER_NAME}
+Company:    ${COMPANY_NAME} - ${COMPANY_WEB}
+Date:       ${DAY} ${MONTH_NAME_SHORT} ${YEAR} ${TIME}
+================================================================*/
